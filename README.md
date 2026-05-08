0eadc246282248bf4d88f9be14dae5cc4247466c
Robert Brune <robert.brune@student.kit.edu>
2026-05-08 17:16:31 +0200

    Fix build.sh and add description to README

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ for [contract-chameleon](https://github.com/Contract-LIB/contract-chameleon).
   - `key-import`
 - Checker Adapters
   - `key-universe-check` (work in progress)
+
+## Project Setup
+
+Just run `./build.sh`,
+which sets up the necessary git submodules,
+and publishes the required dependencies locally.
--- build.sh
+++ build.sh
@@ -3,25 +3,49 @@
 #
 # Build the project.
 #
+# - Script Setup
+# - Fetch Submodules
 # It initialises all submodules if not yet done.
-# It pulls from all submodules
-# It builds the jml parser submodule
-# It cleans and builds the main modules
+# It pulls from all submodules.
+#
+# - Build and Publish Submodules Locally
+# It builds the jml parser submodule and publish it to maven local.
+# It build the base contract-chameleon submodule and publish it to maven local.
+#
+# - Build Adapters
+# It cleans and builds the main modules.
 #
 # See README.md for details
+
+# - Script Setup
+
+# identify the root file of the repository
+ROOT="$(cd "$(dirname "$0")" && pwd)"
+# go to the root directory
+cd "$ROOT"
+
+# - Fetch submodules
 
 # fetch and initialize the submodules the first time
 git submodule update --init --recursive
 
 # pull changes from submodules
-git pull --rebase --recurse-submodules 
+git pull --rebase --recurse-submodules
+
+# - Build and Publish Submodules Locally
+# publish custom jml parser
+cd "$ROOT/libs/jmlparser"
+./mvnw clean install -DskipTests
 
 # publish custom jml parser
-cd libs/jmlparser
-./mvnw clean install -DskipTests
-#check that snapshot exists
-ls ~/.m2/repository/io/github/jmltoolkit/jmlparser-core/ 
-cd ../..
+cd "$ROOT/libs/contract-chameleon"
+./gradlew clean publishToMavenLocal
 
+#check that snapshots exist
+ls ~/.m2/repository/io/github/jmltoolkit/jmlparser-core/
+ls ~/.m2/repository/org/contract_lib/contract-chameleon/
+
+# - Build Adapters
+cd "$ROOT"
 # build with gradle
 ./gradlew clean build
